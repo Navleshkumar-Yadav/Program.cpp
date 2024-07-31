62e7c1ab85a88a389a0b818517a40e16697d2823
Navleshkumar-Yadav <119070031+Navleshkumar-Yadav@users.noreply.github.com>
2024-07-31 08:15:30 +0530

    Create basicprogram.cpp

---
+++ basicprogram.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num = 50;
+    cout<<num<<endl;
+    cout<<sizeof(num)<<endl;
+    char ch = 'K';
+    cout<<ch<<endl;
+    cout<<sizeof(ch)<<endl;
+    float n = 8.9;
+    cout<<n<<endl;
+    cout<<sizeof(n)<<endl;
+    long number = 18;
+    cout<<number<<endl;
+    cout<<sizeof(number)<<endl;
+}
